08e3181321f1e1d1583763445c416740db0353f1
Marcelo <cortmr@gmail.com>
2026-05-22 18:56:05 -0300

    Revert "ajustes"
    This reverts commit 3e2fd111458cb9ca1ab031dcf920cc719504942d.

--- scratch/list_schema.js
+++
@@ -1,5 +0,0 @@
-const db = require("../backend/db/database");
-
-const tables = db.prepare("SELECT name FROM sqlite_master WHERE type='table'").all();
-console.log("Tables in database:");
-console.log(tables.map(t => t.name).join("\n"));
